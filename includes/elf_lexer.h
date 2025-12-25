2cb9456b5efdca0525feb12cae7c21c58fbf0f4b
Shariar Papon <spaponwork@gmail.com>
2025-12-24 22:03:27 -0500

    todo: tokentype issue

--- includes/elf_lexer.h
+++ includes/elf_lexer.h
@@ -4,19 +4,25 @@
 
 typedef enum TokenType
 {
-    IDENTIFIER=0,
-    RETURN,
-    STRING_LITERAL
-
+    INVALID_TOKEN=0,
+    IDENTIFIER,
+    DIGIT,
+    OPERATOR,
+    WHITE_SPACE,
+    DOT,
+    DB_QUOTE,
+    STMT_END,
+    SCOPE_BEG,
+    SCOPE_END,
+    EXPR_BEG,
+    EXPR_END,
+    TERMINATE,
 } t_elf_token_type;
 
 typedef struct Token
 {
+    char c;
     t_elf_token_type type;
-    const char* source;
-    size_t start;
-    size_t end;
-
 } t_elf_token;
 
 typedef struct ElfLexer
@@ -26,8 +32,8 @@ typedef struct ElfLexer
 }t_elf_lexer;
 
 t_elf_lexer* elf_create_lexer(const char* source);
-t_elf_token* elf_create_token(const char* source, size_t start, size_t end);
-t_elf_token_type elf_get_tokentype(const char* source, size_t start, size_t end);
+t_elf_token* elf_create_token(char c, t_token_type type);
+t_elf_token_type elf_get_tokentype(char c);
 
 
 
--- src/elf_lexer.c
+++ src/elf_lexer.c
@@ -18,7 +18,7 @@ t_elf_lexer* elf_create_lexer(const char* source)
     }
 
     lexer->source = source;
-    const size_t token_capacity = 128;
+    const size_t token_capacity = strlen(source);
     lexer->tokens = (t_elf_token**)malloc(sizeof(t_elf_token*) * token_capacity);
     if(!lexer->tokens)
     {
@@ -32,7 +32,7 @@ t_elf_lexer* elf_create_lexer(const char* source)
     return lexer;
 }
 
-t_elf_token* elf_create_token(const char* source, size_t start, size_t end)
+t_elf_token* elf_create_token(char c, t_token_type type)
 {
     t_elf_token* token = (t_elf_token*)malloc(sizeof(t_elf_token));
     if(!token)
@@ -41,16 +41,105 @@ t_elf_token* elf_create_token(const char* source, size_t start, size_t end)
         printf("__error: could not allocate token memory\n");
         return (t_elf_token*){0};
     }
-    
-    token->source = source;
-    token->start = start;
-    token->end = end;
-    token->type = elf_get_tokentype(source, start, end);
+    token->c = c;
+    token->type = type;
     return token;
 }
 
-t_elf_token_type elf_get_tokentype(const char* source, size_t start, size_t end)
+
+t_elf_token_type elf_get_tokentype(char c)
 {
-    return IDENTIFIER;
+    switch(c)
+    {
+        default: 
+        return INVALID_TOKEN;
+        
+        case 'a': case 'b': case 'c': case 'd' : case 'e':
+        case 'f': case 'g': case 'h': case 'i': case 'j':
+        case 'k': case 'l': case 'm': case 'n': case 'o':
+        case 'p': case 'q': case 'r': case 's': case 't':
+        case 'u': case 'v': case 'w': case 'x': case 'y':
+        case 'z': 
+        case 'A': case 'B': case 'C': case 'D': case 'E':
+        case 'G': case 'H': case 'I': case 'J': case 'K':
+        case 'L': case 'M': case 'N': case 'O': case 'P':
+        case 'Q': case 'R': case 'S': case 'T': case 'U':
+        case 'U': case 'V': case 'W': case 'X': case 'Y':
+        case 'Z': 
+        case '_':
+        return IDENTIFIER;
+        
+        case '0': case '1': case '2': case'3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        return DIGIT;
+
+        case ' ':
+        return WHITE_SPACE;
+
+        case '.': 
+        return DOT;
+
+        case '+': case '-': case '*': case '/':
+        case '%': case '!': case '^': case '=':
+        return OPERATOR;
+
+        case '"':
+        return DB_QUOTE;
+
+        case ';':
+        return STMT_END;
+
+        case '{': 
+        return SCOPE_BEG;
+
+        case '}':
+        return SCOPE_END;
+
+        case '(':
+        return EXPR_BEG;
+
+        case ')':
+        return EXPR_END;
+
+        case '\0':
+        return TERMINATE;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
