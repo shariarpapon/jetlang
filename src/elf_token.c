e8688da620cb5fb2283578bd530e50090f83b26c
Shariar Papon <spaponwork@gmail.com>
2026-01-03 00:20:20 -0500

    token enum to str utility func added

--- src/elf_token.c
+++ src/elf_token.c
@@ -45,8 +45,80 @@ void elf_Token_printList(elf_Token** tokens, size_t count)
 
 const char* elf_TokenType_str(elf_TokenType type)
 {
-    return "null";
+    switch(type)
+    {
+        default:          return "unknown";
+        case TOK_INV:     return "TOK_INV";
+        case TOK_EOF:     return "TOK_EOF";
+        
+        case TOK_ID:      return "TOK_ID";
+        case TOK_KW:      return "TOK_KW"; 
+        case TOK_NUM:     return "TOK_NUM";
+        case TOK_STR:     return "TOK_STR";
+        case TOK_BCOM:    return "TOK_BCOM";
+        case TOK_LCOM:    return "TOK_LCOM";
+        
+        case TOK_ASG:     return "TOK_ASG";
+        case TOK_EQ:      return "TOK_EQ";
+        case TOK_NEQ:     return "TOK_NEQ";
+        case TOK_LT:      return "TOK_LT";
+        case TOK_LTE:     return "TOK_LTE";
+        case TOK_GT:      return "TOK_GT";
+        case TOK_GTE:     return "TOK_GTE";
+        case TOK_BAND:    return "TOK_BAND";
+        case TOK_AND:     return "TOK_AND";
+        case TOK_BOR:     return "TOK_BOR";
+        case TOK_OR:      return "TOK_OR";
+        case TOK_XOR:     return "TOK_XOR";
+        case TOK_NOT:     return "TOK_NOT";
+
+        case TOK_PLUS:    return "TOK_PLUS";
+        case TOK_MINUS:   return "TOK_MINUS";
+        case TOK_STAR:    return "TOK_STAR";
+        case TOK_SLASH:   return "TOK_SLASH";
+        case TOK_PERCENT: return "TOK_PERCENT";
+        
+        case TOK_PLUSEQ:  return "TOK_PLUSEQ";
+        case TOK_MINEQ:   return "TOK_MINEQ";
+        case TOK_MULEQ:   return "TOK_MULEQ";
+        case TOK_DIVEQ:   return "TOK_DIVEQ";
+        case TOK_POW:     return "TOK_POW";
+
+        case TOK_SEMI:    return "TOK_SEMI";
+        case TOK_DOT:     return "TOK_DOT";
+        case TOK_LPAR:    return "TOK_LPAR";
+        case TOK_RPAR:    return "TOK_RPAR";
+        case TOK_LBR:     return "TOK_LBR";
+        case TOK_RBR:     return "TOK_RBR";
+        case TOK_LBRK:    return "TOK_LBRK";
+        case TOK_RBRK:    return "TOK_RBRK";
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
