adcad357c9d52c2836c352103620214bfbc54d67
Shariar Papon <spaponwork@gmail.com>
2026-01-18 03:12:38 -0500

    overhaul 2

--- jet_ast_node.h
+++ jet_ast_node.h
@@ -1,13 +1,22 @@
 #pragma once
+#include <jet_list.h>
 
 typedef enum jnode_type
 {
-    
+  jnode_FUNC_DECL,
+  jnode_FUNC_DEF,
+  jnode_VAR_DECL,
 } jnode_type;
 
 typedef struct jnode
 {
-
+    jnode_type node_type;
+    union
+    {
+        jnode_func_decl* func_decl;
+        jnode_func_def* func_def;
+        jnode_var_decl* var_decl;
+    }
 } jnode;
 
 
