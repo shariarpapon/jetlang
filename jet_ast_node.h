52a25f7f649d98806bfb1d432e94628fce57d56a
Shariar Papon <spaponwork@gmail.com>
2026-01-18 22:46:39 -0500

    various ast node types added

--- jet_ast_node.h
+++ jet_ast_node.h
@@ -2,43 +2,76 @@
 #include <jet_list.h>
 #include <jnode_type.h>
 
-typedef struct jnode
-{
-    jnode_type node_type;
-    union
-    {
-        jnode_prog* prog;
-        jnode_block* block;
-        jnode_type_decl* type_decl;
-        jnode_func_def* func_def;
-    }
-} jnode;
-
 // PROGRAM ENTRY POINT
 typedef struct jnode_prog 
 {
-  jet_list* nodes;
+  jnode* block;
 } jnode_prog;
+
+typedef struct jnode_ident
+{
+  const char* value;
+} jnode_ident;
 
 typedef struct jnode_block
 {
   jet_list* nodes;
 } jnode_block;
 
+typedef struct jnode_var_ref 
+{
+  jnode* var_binding_ident;
+} jnode_var_ref;
+
+typedef struct jnode_var_decl
+{  
+  jnode* binding_ident;
+  jnode* type_decl;
+  jnode* init_value;
+} jnode_var_decl;
+
 typedef struct jnode_type_decl 
 {
-  const char* binding_name;
+  jnode* type_ident;
   size_t byte_size;
-  jnode* init_value;
+  bool is_native;
 } jnode_type_decl;
+
+typedef struct jnode_func_decl
+{
+  jnode* binding_ident;
+  jet_list* ret_type_decls;
+  jet_list* param_var_decls; 
+} jnode_func_decl;
 
 typedef struct jnode_func_def
 {
-  const char* binding_name;
-  jet_list* param_nodes; 
+  jnode* func_decl;
   jnode* block;
 } jnode_func_def; 
 
+typedef struct jnode_func_call
+{
+  jnode* func_binding_ident;
+} jnode_func_call;
+
+// NODE BASE STRUCT
+typedef struct jnode
+{
+    jnode_type node_type;
+    union
+    {
+        jnode_prog* prog;
+        jnode_ident* ident;
+        jnode_block* block; 
+        jnode_var_ref* var_ref;
+        jnode_var_decl* var_decl;
+        jnode_type_decl* type_decl;
+        jnode_func_decl* func_decl;
+        jnode_func_def* func_def;
+        jnode_func_call* func_call;
+    } as;
+} jnode;
 
 
 
--- jnode_type.h
+++ jnode_type.h
@@ -3,7 +3,12 @@
 typedef enum jnode_type
 {
   jnode_PROG,  
+  jnode_IDENT,
   jnode_BLOCK,
+  jnode_VAR_REF,
+  jnode_VAR_DECL,
   jnode_TYPE_DECL,
+  jnode_FUNC_DECL,
   jnode_FUNC_DEF,
+  jnode_FUNC_CALL,
 } jnode_type;
