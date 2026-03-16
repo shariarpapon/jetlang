c60fbf51986bf49a1ad4ea9121dcd7c155ee5769
Shariar Papon <spaponwork@gmail.com>
2026-03-16 19:11:34 -0400

    started on logger

---
+++ src/jet_logger.c
@@ -0,0 +1,25 @@
+#include <jet_logger.h>
+
+#include <string.h>
+#include <stdarg.h>
+#include <stdio.h>
+
+#define MSG_BUF_SIZE (1024 * 32) 
+
+void jet_log_output(jet_log_level level, const char* fmt, ...)
+{
+    const char* level_str[] = { "FATAL", "ERROR", "WARNING", "INFO", "DEBUG" };
+    __builtin_va_list args;
+    __builtin_va_start(args, fmt);
+    
+    char msg_buf[MSG_BUF_SIZE];
+    memset(msg_buf, 0, sizeof(msg_buf));
+    vsnprintf(msg_buf, sizeof(msg_buf), fmt, args); 
+    __builtin_va_end(args);
+
+    char out_buf[MSG_BUF_SIZE];
+    memset(out_buf, 0, sizeof(out_buf));
+    snprintf(out_buf, sizeof(out_buf), "%s:%s\n", level_str[level], msg_buf);
+
+    printf("%s", out_buf);
+}
