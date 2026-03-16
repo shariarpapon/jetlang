2fc325c676a55e303e8cd56327507e3d45b4db08
Shariar Papon <spaponwork@gmail.com>
2026-03-16 12:58:27 -0400

    updated

--- include/jet_logger.h
+++ include/jet_logger.h
@@ -11,13 +11,37 @@ typedef enum jet_log_level
 
 void jet_log_output(jet_log_level level, const char* fmt, ...);
 
-#define JET_LOG_ENABLE_WARNING 1
-#define JET_LOG_ENABLE_INFO 1
-#define JET_LOG_ENABLE_DEBUG 1
+#define JET_LOG_ENABLE_WARNING  1
+#define JET_LOG_ENABLE_INFO     1
+#define JET_LOG_ENABLE_DEBUG    1
 
-#define JET_LOG
+#define JET_FATAL(fmt, ...) \
+    jet_log_output(JET_LOG_LEVEL_FATAL, fmt, ##__VA_ARGS__)
 
 
+#define JET_ERROR(fmt, ...) \
+    jet_log_output(JET_LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
 
+
+#if JET_LOG_ENABLE_WARNING == 1
+#define JET_WARNING(fmt, ...) \
+    jet_log_output(JET_LOG_LEVEL_WARNING, fmt, ##__VA_ARGS__)
+#else
+#define JET_WARNING(fmt, ...)
+#endif
+
+#if JET_LOG_ENABLE_INFO == 1
+#define JET_INFO(fmt, ...) \
+    jet_log_output(JET_LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
+#else
+#define JET_INFO(fmt, ...)
+#endif
+
+#if JET_LOG_ENABLE_DEBUG == 1
+#define JET_DEBUG(fmt, ...) \
+    jet_log_output(JET_LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
+#else 
+#define JET_DEBUG(fmt, ...)
+#endif
 
 
