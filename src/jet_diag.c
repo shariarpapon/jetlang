3dc5a942cc92da5995821b51313f83c237f1bc15
Shariar Papon <spaponwork@gmail.com>
2026-03-24 20:37:47 -0400

    started diag overhaul

--- src/jet_diag.c
+++ src/jet_diag.c
@@ -7,7 +7,9 @@
 
 static bool handler_started = false;
 static const char* cur_filename = NULL;
-static size_t err_count = 0;
+static size_t report_count = 0;
+
+#define JET_DIAG_SPAN_HEADER_FMT(span) ""
 
 void jet_diag_start(const char* filename)
 {
@@ -33,64 +35,65 @@ bool jet_diag_is_started() { return handler_started; }
 const char* jet_diag_get_filename() { return cur_filename; }
 size_t jet_diag_get_count() { return err_count; }
 
-void jet_diag_pushf(jet_log_level level, const jet_span* span, const char* fmt, ...) 
+// error emitters
+void jet_diag_emit(jet_log_level level, 
+        const char* filename, 
+        uint32_t line, 
+        uint32_t col, 
+        const char* msg)
 {
-    if(!handler_started)
-    {
-        JET_LOG_ERROR("must start error handler before pushing errors.");
-        return;
-    }
+    jet_log_outputf_flc(level, filename, line, col, "%s", msg);
+    report_count++;
+}
 
-    if(!cur_filename || !fmt || !span)
-    {
-        JET_LOG_ERROR("cannot push error, one or more invalid args.");
-        return;
-    }
-
+void jet_diag_pushf(jet_log_level level, 
+        uint32_t line, 
+        uint32_t col, 
+        const char* fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
-    
     char buf[JET_LOG_MSG_BUF_SIZE];
-    jet_vsnprintf(buf, sizeof(buf), fmt, args);
+    jet_vsnprintf(buf, sizeof(buf), args);
     va_end(args);
-    jet_log_outputf_flc(level, cur_filename, span->line, span->col, "%s", buf);
-    err_count++;
+
+    jet_diag_emit(level, cur_filename, line, col, buf);
 }
 
-void jet_diag_pushf_tok(jet_log_level level, const jet_token* tok, const char*  fmt, ...)
+void jet_diag_pushf_span(jet_log_level level, 
+        const jet_span* span, 
+        const char* fmt, ...)
 {
-    JET_ASSERT(tok != NULL);
-    JET_ASSERT(level >= JET_LOG_LEVEL_FATAL && level <= JET_LOG_LEVEL_DEBUG);
-
-    va_list args;
-    va_start(args, fmt);
-    char msg_buf[JET_LOG_MSG_BUF_SIZE];
-    jet_vsnprintf(msg_buf, sizeof(msg_buf), fmt, args);
-    va_end(args);
-    
-    const char* tok_type_str = jet_token_type_str(tok->type); 
-    char* tok_strdup;
-    jet_token_strdup(tok, &tok_strdup);
-    jet_diag_pushf(level, &tok->span, "token[%s, %s] %s", tok_type_str, tok_strdup, msg_buf);
-    free(tok_strdup);
 }
 
-void jet_diag_pushf_node(jet_log_level level, const jet_ast_node* node, const char*  fmt, ...)
+void jet_diag_pushf_tok(jet_log_level level, 
+        const jet_token* tok, 
+        const char* fmt, ...)
 {
-    JET_ASSERT(node != NULL);
-    JET_ASSERT(level >= JET_LOG_LEVEL_FATAL && level <= JET_LOG_LEVEL_DEBUG);
 
-    va_list args;
-    va_start(args, fmt);
-    char msg_buf[JET_LOG_MSG_BUF_SIZE];
-    jet_vsnprintf(msg_buf, sizeof(msg_buf), fmt, args);
-    va_end(args);
-    
-    const char* node_type_str = jet_ast_node_type_str(node->node_type); 
-    jet_diag_pushf(level, &node->span, "node[%s] %s", node_type_str, msg_buf);
+    jet_diag_pushf_span(level, );
 }
 
-void jet_diag_pushf_cursor(jet_log_level level, size_t cursor, const char* msg)
+void jet_diag_pushf_node(jet_log_level level, 
+        const jet_ast_node* node, 
+        const char* fmt, ...)
 {
-    jet_log_outputf_fl(level, cur_filename, 0, "[cursor:%zu] %s", cursor, msg);
 }
+
+void jet_diag_expected_token(
+        const jet_token* tok, 
+        jet_token_type expected)
+{
+}
+
+void jet_diag_unexpected_token(const jet_token* tok)
+{
+}
+
+void jet_diag_missing(
+        const jet_token* anchor, 
+        const char* entity)
+{
+
+}
+
